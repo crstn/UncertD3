ca3ce5ecebf234e3d693c62de5001709ded344c2
Carsten Keßler <carsten.kessler@hunter.cuny.edu>
2016-02-16 16:33:39 -0500

    Scale adjusted (1..7)

--- js/common.js
+++ js/common.js
@@ -20,13 +20,13 @@ var opacity = d3.scale.linear()
       .range([1, 0.3]);
 
 function randomize(cardata){
-      // replace accuracy with random values [1..6]
+      // replace accuracy with random values [2..6]
       for(var d in cardata.features){
-          cardata.features[d].properties.accuracy = Math.floor((Math.random() * 6)) + 1;
+          cardata.features[d].properties.accuracy = Math.floor((Math.random() * 5)) + 2;
       }
 
-      // Then set one random element to min (0) and max (7)
-      cardata.features[Math.floor((Math.random() * cardata.features.length + 1))].properties.accuracy = 0;
+      // Then set one random element to min (1) and max (7)
+      cardata.features[Math.floor((Math.random() * cardata.features.length + 1))].properties.accuracy = 1;
       cardata.features[Math.floor((Math.random() * cardata.features.length + 1))].properties.accuracy = 7;
 
       return cardata;

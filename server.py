7dbcb8af2427352848847ce79c174560bed0c737
Carsten Keßler <carsten.kessler@hunter.cuny.edu>
2016-10-27 11:59:23 +0200

    Set everything up to work in remote server

--- server.py
+++ server.py
@@ -10,15 +10,10 @@ import os
 import cgitb
 cgitb.enable()
 
-logging.warning("======= POST STARTED =======")
+# logging.warning("======= POST STARTED =======")
 # logging.warning(self.headers)
-form = cgi.FieldStorage(
-    fp=self.rfile,
-    headers=self.headers,
-    environ={'REQUEST_METHOD':'POST',
-             'CONTENT_TYPE':self.headers['Content-Type'],
-             })
-logging.warning("======= POST VALUES =======")
+form = cgi.FieldStorage()
+# logging.warning("======= POST VALUES =======")
 # for item in form.list:
 #     logging.warning(item)
 
@@ -26,11 +21,23 @@ session = form.getvalue('session')
 
 directory = 'experiments/'+session
 
-if not os.path.exists(directory):
-    os.makedirs(directory)
+
+try:
+    if not os.path.exists(directory):
+        os.makedirs(directory, 0777)
+    else:
+        logging.warning(directory + " exists")
+        os.chmod(directory, 0777)   # just in case
+except Exception as e:
+    logging.error(type(e))
+    logging.error(e.args)
+    logging.error(e)
 
 # this is to process the final questionnaire:
 if(form.getvalue('q') == 't'):
+
+    csv = "experiments/questionnaires.csv"
+
     header = ""
     values = ""
 
@@ -38,22 +45,31 @@ if(form.getvalue('q') == 't'):
         header += str(key)+", "
         values += str(form.getvalue(str(key)))+", "
 
-    with open("experiments/questionnaires.csv", "a") as text_file:
+    with open(csv, "a") as text_file:
             text_file.write(header+"\n")
             text_file.write(values+"\n")
 
-    # redirect to thank you page
-    self.send_response(301)
-    self.send_header('Location','http://localhost:8000/thanks.html')
-    self.end_headers()
+    os.chmod(csv, 0777)
 
-# this process the randomized car json datasets
+    print 'Content-Type: text/html'
+    print 'Status: 200 OK'
+    print
+    print '<!DOCTYPE html>'
+    print '<meta charset="utf-8">'
+    print '<link rel="stylesheet" href="css/pure-min.css" />'
+    print '<link rel="stylesheet" href="css/style.css" />'
+    print '<body> <h1>Thank you.</h1> </body>'
+
+# this processes the randomized car json datasets
 else:
     data = form.getvalue('data')
     page = form.getvalue('page')
 
-    with open(directory+"/"+page+".json", "w") as text_file:
+    json = directory+"/"+page+".json"
+
+    with open(json, "w") as text_file:
         text_file.write(data)
 
-    logging.warning("\n")
+    os.chmod(json, 0777)
+
     SimpleHTTPServer.SimpleHTTPRequestHandler.do_GET(self)

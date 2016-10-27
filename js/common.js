e4ed3d00a663b9082259796dc1bb6eb30c729b50
Carsten Keßler <carsten.kessler@hunter.cuny.edu>
2016-10-27 14:45:08 +0200

    First stab at logging selected positions and time

--- js/common.js
+++ js/common.js
@@ -1,4 +1,4 @@
-var width  = 720,
+var width = 720,
     height = 720;
 
 var scale = 530000;
@@ -6,49 +6,68 @@ var offset = [width / 2 + 22850, height / 2 - 9170];
 var center = [-71.47496712465082, 41.525055483742605]
 
 var projection = d3.geo.mercator()
-  .scale(scale)
-  .center(center)
-  .translate(offset);
+    .scale(scale)
+    .center(center)
+    .translate(offset);
 
 var car = d3.select("#js-geojson-example").append("svg")
-      .attr("width", width)
-      .attr("height", height);
+    .attr("width", width)
+    .attr("height", height);
+
 
 // add a rectangle to see the bound of the svg
 car.append("rect")
-  .attr('width', width)
-  .attr('height', height)
-  .style('stroke', 'black')
-  .style('fill', 'none');
+    .attr('width', width)
+    .attr('height', height)
+    .style('stroke', 'black')
+    .style('fill', 'none');
 
 // function to scale bubble size based on data (GPS accuracy)
 var radius = d3.scale.linear()
-      .domain([1, 7])
-      .range([3, 10]);
+    .domain([1, 7])
+    .range([3, 10]);
 
 // function to scale bubble opacity based on data (GPS accuracy)
 var opacity = d3.scale.linear()
-      .domain([1, 7])
-      .range([1, 0.3]);
+    .domain([1, 7])
+    .range([1, 0.3]);
 
-function randomize(cardata){
-      // replace accuracy with random values [2..6]
-      for(var d in cardata.features){
-          cardata.features[d].properties.accuracy = Math.floor((Math.random() * 5)) + 2;
-      }
+function randomize(cardata) {
+    // replace accuracy with random values [2..6]
+    for (var d in cardata.features) {
+        cardata.features[d].properties.accuracy = Math.floor((Math.random() * 5)) + 2;
+    }
 
-      // Then set one random element to min (1) and max (7)
-      var minIndex = Math.floor((Math.random() * cardata.features.length))
-      console.log(minIndex)
-      cardata.features[minIndex].properties.accuracy = 1;
+    // Then set one random element to min (1) and max (7)
+    var minIndex = Math.floor((Math.random() * cardata.features.length))
+    console.log(minIndex)
+    cardata.features[minIndex].properties.accuracy = 1;
 
-      // make sure we don't change the same as above, i.e. minIndex needs to be different from maxIndex
-      var maxIndex = minIndex
-      while(maxIndex == minIndex){
-         maxIndex = Math.floor((Math.random() * cardata.features.length))
-         console.log(maxIndex)
-      }
-      cardata.features[maxIndex].properties.accuracy = 7;
+    // make sure we don't change the same as above, i.e. minIndex needs to be different from maxIndex
+    var maxIndex = minIndex
+    while (maxIndex == minIndex) {
+        maxIndex = Math.floor((Math.random() * cardata.features.length))
+        console.log(maxIndex)
+    }
+    cardata.features[maxIndex].properties.accuracy = 7;
 
-      return cardata;
+    return cardata;
+}
+
+function addListener(target) {
+    var start = Date.now()
+    var svg = d3.select("svg");
+    svg.on("click", function() {
+        console.log(d3.mouse(svg.node()));
+
+        var pos = d3.mouse(svg.node()),
+            px = pos[0],
+            py = pos[1];
+
+        var coords = projection.invert([px, py]);
+
+        console.log(coords);
+        console.log(Date.now() - start)
+        // window.location.href = target;
+    });
 }

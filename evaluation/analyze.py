583368c5483771b7a6ffe08d3e606b90fe3cddf9
Carsten Keßler <carsten.kessler@hunter.cuny.edu>
2016-10-31 22:52:12 +0100

    More pie charts

--- evaluation/analyze.py
+++ evaluation/analyze.py
@@ -15,16 +15,8 @@ def gatherPieData(dic, key):
 
     return labels, counts
 
-# def calculatePieSizes(counts):
-#     print sum(counts)
-#
-#     for c in counts:
-
-
-
-
 responses = []
-folder = os.path.expanduser("~")+"/Dropbox/Code/UncertD3/evaluation/data/"
+os.chdir(os.path.expanduser("~")+"/Dropbox/Code/UncertD3/evaluation/data/")
 
 # download the CSV with the up-to-date interview responses to have a backup
 response = urllib.urlopen("http://carsten.io/uncertainty/experiments/questionnaires.csv")
@@ -36,11 +28,11 @@ text = response.read()
 text = text.replace(", ", ",").replace(",\n", "\n")
 
 # save to local file as backup:
-with open(folder+"questionnaires.csv", "w") as text_file:
+with open("questionnaires.csv", "w") as text_file:
     text_file.write(text)
 
 # then read in this local csv file as dict
-with open(folder+"questionnaires.csv", 'rb') as csvfile:
+with open("questionnaires.csv", 'rb') as csvfile:
     # r = csv.reader(csvfile, delimiter=',', quotechar='|')
     rs = csv.DictReader(csvfile, delimiter=',')
     for r in rs:
@@ -49,27 +41,48 @@ with open(folder+"questionnaires.csv", 'rb') as csvfile:
 # next, go through all the sessions and check if we already have a copy of the data for that session;
 # if not, download the data:
 for r in responses:
-    if not os.path.exists(folder+str(r["session"])):
+    if not os.path.exists(str(r["session"])):
         print "Downloading new data for session " +str(r["session"])+ "..."
-        os.makedirs(folder+str(r["session"]))
+        os.makedirs(str(r["session"]))
         for i in range(1,12):
             path = str(r["session"])+"/"+str(i)+".json"
-            urllib.urlretrieve ("http://carsten.io/uncertainty/experiments/"+path, folder+path)
+            urllib.urlretrieve ("http://carsten.io/uncertainty/experiments/"+path, path)
             path = str(r["session"])+"/log.txt"
-            urllib.urlretrieve ("http://carsten.io/uncertainty/experiments/"+path, folder+path)
+            urllib.urlretrieve ("http://carsten.io/uncertainty/experiments/"+path, path)
 
 # on to the actual analysis:
 # first, some pie charts
-
 matplotlib.style.use("fivethirtyeight")
 colors = ['#008fd5', '#fc4f30', '#e5ae38', '#6d904f', '#8b8b8b', '#810f7c']
+
+# gender:
 
 # gather the data
 labels, counts = gatherPieData(responses, "gender")
 total = sum(counts)
 
-plt.pie(counts, labels=labels, colors=colors,
-        autopct=lambda(p): '{:.0f}'.format(p * total / 100), shadow=False, startangle=90, explode = (0.05, 0))
+plt.pie(counts, labels=labels, colors=colors, autopct=lambda(p): '{:.0f}'.format(p * total / 100), shadow=False, startangle=90, explode = (0.05, 0))
 # Set aspect ratio to be equal so that pie is drawn as a circle.
 plt.axis('equal')
-plt.show()
+plt.suptitle('Gender breakdown')
+plt.savefig("../plots/gender.pdf")
+
+
+
+plt.clf()
+
+# best and worst viz types
+
+for t in ["bestmost", "bestleast", "worstmost", "worstleast"]:
+
+    labels, counts = gatherPieData(responses, t)
+    total = sum(counts)
+
+    plt.pie(counts, labels=labels, colors=colors, autopct=lambda(p): '{:.0f}'.format(p * total / 100), shadow=False, startangle=90)
+
+    plt.axis('equal')
+    # plt.suptitle('Best visualization to identify most uncertain object')
+    plt.savefig("../plots/"+t+".pdf")
+
+
+plt.clf()

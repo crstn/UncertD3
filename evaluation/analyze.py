6e4d5dd46f974aa603323bdef992a11181601f42
Carsten Keßler <carsten.kessler@hunter.cuny.edu>
2016-10-31 22:28:37 +0100

    First pie chart added

--- evaluation/analyze.py
+++ evaluation/analyze.py
@@ -1,7 +1,31 @@
-import urllib, os, csv
+import urllib, os, csv, matplotlib
+import matplotlib.pyplot as plt
+
+# some helper functions:
+def gatherPieData(dic, key):
+    labels = []
+    counts = []
+    for row in dic:
+        if row[key] in labels:
+            i = labels.index(row[key])
+            counts[i] = counts[i] + 1
+        else:
+            labels.append(row[key])
+            counts.append(1)
+
+    return labels, counts
+
+# def calculatePieSizes(counts):
+#     print sum(counts)
+#
+#     for c in counts:
+
+
+
 
 responses = []
 folder = os.path.expanduser("~")+"/Dropbox/Code/UncertD3/evaluation/data/"
+
 # download the CSV with the up-to-date interview responses to have a backup
 response = urllib.urlopen("http://carsten.io/uncertainty/experiments/questionnaires.csv")
 
@@ -22,15 +46,30 @@ with open(folder+"questionnaires.csv", 'rb') as csvfile:
     for r in rs:
         responses.append(r)
 
-# next, go through all the sessions and check if we already have a copy of the data for that session:
+# next, go through all the sessions and check if we already have a copy of the data for that session;
+# if not, download the data:
 for r in responses:
-    if os.path.exists(folder+str(r["session"])):
-        print "Data for session " +str(r["session"])+ " already downloaded."
-    else:
-        print "Downloading data for session " +str(r["session"])+ "..."
+    if not os.path.exists(folder+str(r["session"])):
+        print "Downloading new data for session " +str(r["session"])+ "..."
         os.makedirs(folder+str(r["session"]))
         for i in range(1,12):
             path = str(r["session"])+"/"+str(i)+".json"
             urllib.urlretrieve ("http://carsten.io/uncertainty/experiments/"+path, folder+path)
             path = str(r["session"])+"/log.txt"
             urllib.urlretrieve ("http://carsten.io/uncertainty/experiments/"+path, folder+path)
+
+# on to the actual analysis:
+# first, some pie charts
+
+matplotlib.style.use("fivethirtyeight")
+colors = ['#008fd5', '#fc4f30', '#e5ae38', '#6d904f', '#8b8b8b', '#810f7c']
+
+# gather the data
+labels, counts = gatherPieData(responses, "gender")
+total = sum(counts)
+
+plt.pie(counts, labels=labels, colors=colors,
+        autopct=lambda(p): '{:.0f}'.format(p * total / 100), shadow=False, startangle=90, explode = (0.05, 0))
+# Set aspect ratio to be equal so that pie is drawn as a circle.
+plt.axis('equal')
+plt.show()

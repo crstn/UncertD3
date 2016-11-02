a60c3c7ef21f2e1bebc98d5f41a6b91280f6a27d
Carsten Keßler <carsten.kessler@hunter.cuny.edu>
2016-11-02 23:26:06 +0100

    Even more pie plots...

--- evaluation/analyze.py
+++ evaluation/analyze.py
@@ -1,12 +1,25 @@
-import urllib, os, csv, matplotlib, json, geopy, pprint, sys
+import urllib
+import os
+import csv
+import matplotlib
+import json
+import geopy
+import pprint
+import sys
 import matplotlib.pyplot as plt
 import numpy as np
 from geopy.distance import vincenty
 
+# some constants:
+circleRadius = 300
+idwNeighbors = 5
+
 # some helper functions:
+
 def getDist(pointA, pointB):
     # geojson uses x/y, geopy uses lat/lon, so we have to flip the coordinates:
     return vincenty((pointA[1], pointA[0]), (pointB[1], pointB[0])).meters
+
 
 # counts the occurances of a certain key in a dictionary (dic).
 def gatherPieData(dic, key):
@@ -23,6 +36,8 @@ def gatherPieData(dic, key):
     return sortCounts(labels, counts)
 
 # puts the labels and corresponding counts in alphabetic order
+
+
 def sortCounts(labels, counts):
     newlabels = []
     newcounts = []
@@ -30,7 +45,7 @@ def sortCounts(labels, counts):
     # add leading zeros so that the sorting works:
     for i in range(len(labels)):
         if len(labels[i]) == 1:
-            labels[i] = "0"+labels[i]
+            labels[i] = "0" + labels[i]
 
     # repeat until the input list is empty:
     while labels:
@@ -49,12 +64,14 @@ def sortCounts(labels, counts):
 
 # The limit will cap the response time at x milliseconds in order not to screw up the box plots later.
 # Default is 80 seconds.
-def getresponsetimes(responses, limit = 80000):
+
+
+def getresponsetimes(responses, limit=80000):
 
     responsetimes = []
 
     for r in responses:
-        logfile = str(r["session"])+'/log.txt'
+        logfile = str(r["session"]) + '/log.txt'
         with open(logfile) as f:
             lines = f.readlines()
 
@@ -65,9 +82,10 @@ def getresponsetimes(responses, limit = 80000):
                 if time > limit:
                     time = limit
 
-                # our pages start at 1, but the list indices at zero, so always subtract 1:
-                if len(responsetimes) > page-1:
-                    responsetimes[page-1].append(time)
+                # our pages start at 1, but the list indices at zero, so always
+                # subtract 1:
+                if len(responsetimes) > page - 1:
+                    responsetimes[page - 1].append(time)
                 else:
                     responsetimes.append([time])
 
@@ -77,9 +95,17 @@ def getresponsetimes(responses, limit = 80000):
 # Loads the log.txt files into one big dictionary
 def getClicks():
     clicks = {}
+    id = 0
+
+    # We'll also write all of the point to a csv file
+    # (use it to quickly create a heat map in Carto)
+    csv = "allclicks.csv"
+    with open(csv, "w") as text_file:
+        text_file.write("id,session,page,lon,lat\n")
+
     for r in responses:
         s = str(r["session"])
-        logfile = s+'/log.txt'
+        logfile = s + '/log.txt'
 
         with open(logfile) as f:
             lines = f.readlines()
@@ -98,12 +124,17 @@ def getClicks():
                 clicks[s][parts[0]]["lon"] = float(parts[2])
                 clicks[s][parts[0]]["lat"] = float(parts[3])
 
-    # pprint.pprint(clicks)
+                # and write the same stuff to the csv
+                with open(csv, "a") as text_file:
+                    text_file.write(str(id)+","+s+","+parts[0]+","+parts[2]+","+parts[3])
+
+                id = id+1
+
     return clicks
 
 
 def openGeoJSON(session, page):
-    with open(str(session)+"/"+str(page)+".json") as f:
+    with open(str(session) + "/" + str(page) + ".json") as f:
         data = json.load(f)
         # pprint.pprint(data)
         return data
@@ -112,16 +143,20 @@ def openGeoJSON(session, page):
 def getCoordsByValue(session, page, value):
     data = openGeoJSON(session, page)
     for feature in data['features']:
-        # Careful here: high accuracy values actually mean high uncertainty. Don't ask me why I did it that way...
+        # Careful here: high accuracy values actually mean high uncertainty.
+        # Don't ask me why I did it that way...
         if feature['properties']['accuracy'] == value:
             return feature['geometry']['coordinates']
 
-    # return an empty array if no point with accuracy of 'value' is found, which should never happen
+    # return an empty array if no point with accuracy of 'value' is found,
+    # which should never happen
     print "No point found... this shouldn't happen!"
     return []
 
+
 def getMostUncertain(session, page):
     return getCoordsByValue(session, page, 7)
+
 
 def getLeastUncertain(session, page):
     return getCoordsByValue(session, page, 1)
@@ -130,15 +165,30 @@ def getLeastUncertain(session, page):
 def getClosest(session, page, clickpoint):
     return getIDW(session, page, clickpoint, 1)
 
+# dist defaults to circleRadius set at the top. 200m is roughly the real-world radius of our
+# circle in the test
 
-# gets an inverse distance weighted average uncertainty at clickpoint using its n nearest neighbors
+
+def getNumOfPointsWithinDistance(session, page, clickpoint, maxDist=circleRadius):
+    data = openGeoJSON(session, page)
+    distances = np.array([])
+    for feature in data['features']:
+        d = getDist(feature['geometry']['coordinates'], clickpoint)
+        distances = np.append(distances, [d])
+
+    return distances[distances <= maxDist].size
+
+
+# gets an inverse distance weighted average uncertainty at clickpoint
+# using its n nearest neighbors
 def getIDW(session, page, clickpoint, n):
     data = openGeoJSON(session, page)
 
     distances = np.array([])
     uncertainties = np.array([])
 
-    # put all the distances in one array, and the accuracy values into another one:
+    # put all the distances in one array, and the accuracy values into another
+    # one:
     for feature in data['features']:
 
         d = getDist(feature['geometry']['coordinates'], clickpoint)
@@ -160,20 +210,20 @@ def getIDW(session, page, clickpoint, n):
 
     return iwd
 
-
+# let's start the actual work...
 
 responses = []
-os.chdir(os.path.expanduser("~")+"/Dropbox/Code/UncertD3/evaluation/data/")
+os.chdir(os.path.expanduser("~") + "/Dropbox/Code/UncertD3/evaluation/data/")
 
-sys.exit();
 
 # download the CSV with the up-to-date interview responses to have a backup
-response = urllib.urlopen("http://carsten.io/uncertainty/experiments/questionnaires.csv")
+response = urllib.urlopen(
+    "http://carsten.io/uncertainty/experiments/questionnaires.csv")
 
 # download csv to string
 text = response.read()
 
-#fix formatting of csv:
+# fix formatting of csv:
 text = text.replace(", ", ",").replace(",\n", "\n")
 
 # save to local file as backup:
@@ -193,13 +243,15 @@ print "n = " + str(len(responses))
 # if not, download the data:
 for r in responses:
     if not os.path.exists(str(r["session"])):
-        print "Downloading new data for session " +str(r["session"])+ "..."
+        print "Downloading new data for session " + str(r["session"]) + "..."
         os.makedirs(str(r["session"]))
-        for i in range(1,12):
-            path = str(r["session"])+"/"+str(i)+".json"
-            urllib.urlretrieve ("http://carsten.io/uncertainty/experiments/"+path, path)
-            path = str(r["session"])+"/log.txt"
-            urllib.urlretrieve ("http://carsten.io/uncertainty/experiments/"+path, path)
+        for i in range(1, 12):
+            path = str(r["session"]) + "/" + str(i) + ".json"
+            urllib.urlretrieve(
+                "http://carsten.io/uncertainty/experiments/" + path, path)
+            path = str(r["session"]) + "/log.txt"
+            urllib.urlretrieve(
+                "http://carsten.io/uncertainty/experiments/" + path, path)
 
 # on to the actual analysis:
 # first, some pie charts
@@ -212,7 +264,8 @@ ftecolors = ['#fc4f30', '#008fd5', '#e5ae38', '#6d904f', '#8b8b8b', '#810f7c']
 labels, counts = gatherPieData(responses, "gender")
 total = sum(counts)
 
-plt.pie(counts, labels=labels, colors=ftecolors, autopct=lambda(p): '{:.0f}'.format(p * total / 100), shadow=False, startangle=90)
+plt.pie(counts, labels=labels, colors=ftecolors, autopct=lambda(
+    p): '{:.0f}'.format(p * total / 100), shadow=False, startangle=90)
 # Set aspect ratio to be equal so that pie is drawn as a circle.
 plt.axis('equal')
 plt.suptitle('Gender breakdown')
@@ -227,19 +280,20 @@ d = {"bestmost": "Best visualization to identify the most uncertain object",
      "worstmost": "Worst visualization to identify the most uncertain object",
      "worstleast": "Worst visualization to identify the least uncertain object"}
 
-colors = ['#008fd5', '#33bbff', '#005f8f', '#fc4322', '#B51D03', '#FB310E', '#DD2403', '#C92103', '#fc4f30', '#A11A02', '#F12704', '#8b8b8b']
+colors = ['#008fd5', '#33bbff', '#005f8f', '#fc4322', '#B51D03', '#FB310E',
+          '#DD2403', '#C92103', '#fc4f30', '#A11A02', '#F12704', '#8b8b8b']
 
 for key, value in d.iteritems():
 
     labels, counts = gatherPieData(responses, key)
     total = sum(counts)
 
-    plt.pie(counts, labels=labels, colors=colors, autopct=lambda(p): '{:.0f}'.format(p * total / 100), shadow=False, startangle=90)
+    plt.pie(counts, labels=labels, colors=colors, autopct=lambda(
+        p): '{:.0f}'.format(p * total / 100), shadow=False, startangle=90)
 
     plt.axis('equal')
     plt.suptitle(value)
-    plt.savefig("../plots/"+key+".pdf")
-
+    plt.savefig("../plots/" + key + ".pdf")
 
     plt.clf()
 
@@ -264,11 +318,16 @@ plt.savefig("../plots/responsetimes.pdf")
 
 plt.clf()
 
-# Let's take a look at the GeoJSON files and click locations. First, load the click logs:
-clicks = getClicks();
+# Let's take a look at the GeoJSON files and click locations. First, load
+# the click logs:
+clicks = getClicks()
 
 distances = []
 clostests = []
+idws = []
+idwsWithinDistance = []
+
+lostclicks = 0
 
 # go through all sessions:
 for session in clicks:
@@ -280,7 +339,7 @@ for session in clicks:
         clickpoint = [s[page]['lon'], s[page]['lat']]
         page = int(page)
 
-        if page % 2 == 0: # even
+        if page % 2 == 0:  # even
             # look for the least uncertain object in the corresponding geojson
             maxP = getLeastUncertain(session, page)
         else:  # not even
@@ -291,9 +350,10 @@ for session in clicks:
         d = getDist(maxP, clickpoint)
 
         # collect all distances in a list of lists by page
-        # our pages start at 1, but the list indices at zero, so always subtract 1:
-        if len(distances) > page-1:
-            distances[page-1].append(d)
+        # our pages start at 1, but the list indices at zero, so always
+        # subtract 1:
+        if len(distances) > page - 1:
+            distances[page - 1].append(d)
         else:
             distances.append([d])
 
@@ -301,11 +361,29 @@ for session in clicks:
         # look for the closest point to the click and check its uncertainty
         c = getClosest(session, page, clickpoint)
         if len(clostests) > page - 1:
-            clostests[page-1].append(c)
+            clostests[page - 1].append(c)
         else:
             clostests.append([c])
 
+        # collect all IDW values in a list of lists by page
+        idw = getIDW(session, page, clickpoint, idwNeighbors)
+        if len(idws) > page - 1:
+            idws[page - 1].append(idw)
+        else:
+            idws.append([idw])
 
+        # collect all IDW values for clicks that are within 200m of our track
+        n = getNumOfPointsWithinDistance(session, page, clickpoint)
+        if n > 0:
+            idw = getIDW(session, page, clickpoint, n)
+            if len(idwsWithinDistance) > page - 1:
+                idwsWithinDistance[page - 1].append(idw)
+            else:
+                idwsWithinDistance.append([idw])
+        else:
+            lostclicks = lostclicks + 1
+
+print str(lostclicks) + " out of " + str(len(responses)*11) + " outside of circle (" + str(lostclicks/len(responses)*10.0) + "%)."
 
 # make a box plot of the distances:
 plt.boxplot(distances)
@@ -322,21 +400,36 @@ plt.savefig("../plots/clostest.pdf")
 
 plt.clf()
 
+# and another one, this time with IDW(5):
+plt.boxplot(idws)
+plt.suptitle("IDW uncertainty of 5 closest points")
+plt.savefig("../plots/idw5.pdf")
+
+plt.clf()
 
 
-# iterate through all individual participant responses:
-# for r in responses:
-#     for i in range(1, 12):
-#         try:
-#             with open(str(r["session"])+"/"+str(i)+".json") as f:
-#                 data = json.load(f)
-#         except Exception as e:
-#             print str(r["session"])+"/"+str(i)+".json"
-#             print e
+# and another IDW one, this time only looking at clicks that have points within the circle:
+plt.boxplot(idwsWithinDistance)
+plt.suptitle("IDW uncertainty of points within circle")
+plt.savefig("../plots/idw-within-circle.pdf")
 
-        # for feature in data['features']:
-        #     print feature['properties']['accuracy']
-        #     coords = feature['geometry']['coordinates']
-            # print Point(coords[0],coords[1]).distance(Point(coords[0],coords[1]))
-    # path = str(r["session"])+"/log.txt"
-    # urllib.urlretrieve ("http://carsten.io/uncertainty/experiments/"+path, path)
+plt.clf()
+
+
+# pie plots of the answers to the rest of the questionnaire:
+
+plts = ["size", "sizeinter", "transparency", "transparencyinter", "movement", "movementinter", "combined", "job"]
+questions = ['In the visualizations that used varying symbol sizes, I was under the impression that symbol size reflects...', 'In my interpretation, larger symbols reflect...', 'In the visualizations that used varying transparency, I was under the impression that transparency reflects...', 'In my interpretation, more transparent symbols reflect...', 'In the visualizations that used movement, I was under the impression that movement reflects...', 'In my interpretation, more movement reflects...', 'In my interpretation, the visualizations that combined at least two of these (size, transparency, movement) ...', 'You deal with maps, GIS, etc. at work and/or college']
+
+for i in range(len(plts)):
+    labels, counts = gatherPieData(responses, plts[i])
+    total = sum(counts)
+
+    plt.pie(counts, labels=labels, colors=ftecolors, autopct=lambda(
+        p): '{:.0f}'.format(p * total / 100), shadow=False, startangle=90)
+
+    plt.axis('equal')
+    plt.suptitle(questions[i])
+    plt.savefig("../plots/"+plts[i]+".pdf")
+
+    plt.clf()
